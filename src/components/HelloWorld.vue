b0144446e1f075110ed16416ede922bded031e5c
Syam krishnan k k <86349818+Syamkrishnankk@users.noreply.github.com>
2024-07-19 18:22:51 +0530

    Add files via upload

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,150 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive
+      class="align-centerfill-height mx-auto"
+      max-width="900"
+    >
+    <h2 align="center" class="mb-3">Age Calculator</h2>
+    <v-row align="center" justify="center">
+      <v-card class="ma-5 px-5 pt-6 pb-5"  max-width="400" color="blue-grey-darken-4">
+      <v-text-field
+            v-model="getDate"
+            label="Day"
+            type="number"
+            variant="outlined"
+            clearable
+            style="color: white;"
+            
+          >
+          </v-text-field>
+          <v-text-field
+            v-model="getMonth"
+            label="Month"
+            type="number"
+            variant="outlined"
+            clearable
+            style="color: white;"
+           
+          >
+          </v-text-field>
+          <v-text-field
+            v-model="getYear"
+            label="Year"
+            type="number"
+            variant="outlined"
+            clearable
+            style="color: white;"
+            
+          >
+          </v-text-field>
+          <P align="center" class="mb-4" v-if="isTrue">
+            <span style="color: #72c0ff;font-weight: 600;">{{ ageYear }}</span> years 
+             <span style="color: #72c0ff;font-weight: 600;">{{ ageMonth }}</span> months 
+             <span style="color: #72c0ff;font-weight: 600;">{{ ageDate }}</span> days</P>
+          <v-btn 
+          @click="ageCalc"
+          color="indigo"
+          class="w-100"
+          style="text-transform: unset !important;"
+          >Calculate Age</v-btn>
+    </v-card>
+    </v-row>
+
+     
+    </v-responsive>
+  </v-container>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+  const d = new Date();
+  var year = Number(d.getFullYear());
+  var month = Number(d.getMonth()+1);
+  var date = Number(d.getDate());
+  var ageYear = ref('');
+  var ageMonth = ref('');
+  var ageDate = ref('');
+  const getDate = ref('');
+  const getMonth = ref('');
+  const getYear = ref('');
+  var isTrue = ref(false)
+  const ageCalc = () =>{
+    if(Number(getMonth.value) > 12 || Number(getDate.value) > 31 || Number(getYear.value) > year){
+      window.alert("invalid input");
+      console.log(getYear.value)
+      console.log(getMonth.value)
+      console.log(getDate.value)
+    }
+    else if(Number(getYear.value) == year){
+      if(Number(getMonth.value) > month || Number(getDate.value) > date){
+        window.alert("invalid input");
+      }
+      else{
+        if(getMonth.value.length > 0 && getDate.value.length > 0 && getYear.value.length > 0){
+      isTrue.value = true;
+    }
+    if(month>=Number(getMonth.value)){
+      if(date>=Number(getDate.value)){
+        ageYear.value = year-Number(getYear.value);
+        ageMonth.value = month-Number(getMonth.value);
+        ageDate.value = date-Number(getDate.value);
+      }
+      else{
+        ageYear.value = year-Number(getYear.value);
+        ageMonth.value = month-Number(getMonth.value)-1;
+        ageDate.value = 30-(Number(getDate.value)-date);
+      }
+  }
+  else{
+    if(date>=Number(getDate.value)){
+        ageYear.value = year-Number(getYear.value)-1;
+        ageMonth.value = month+(12-Number(getMonth.value));
+        ageDate.value = date-Number(getDate.value);
+      }
+      else{
+        ageYear.value = year-Number(getYear.value)-1;
+        ageMonth.value = month+(12-Number(getMonth.value));
+        ageDate.value = 30-(Number(getDate.value)-date);
+      }
+        
+  }
+      }
+      
+    }
+    else{
+    if(getMonth.value.length > 0 && getDate.value.length > 0 && getYear.value.length > 0){
+      isTrue.value = true;
+    }
+    if(month>=Number(getMonth.value)){
+      if(date>=Number(getDate.value)){
+        ageYear.value = year-Number(getYear.value);
+        ageMonth.value = month-Number(getMonth.value);
+        ageDate.value = date-Number(getDate.value);
+      }
+      else{
+        ageYear.value = year-Number(getYear.value);
+        ageMonth.value = month-Number(getMonth.value)-1;
+        ageDate.value = 30-(Number(getDate.value)-date);
+      }
+  }
+  else{
+    if(date>=Number(getDate.value)){
+        ageYear.value = year-Number(getYear.value)-1;
+        ageMonth.value = month+(12-Number(getMonth.value));
+        ageDate.value = date-Number(getDate.value);
+      }
+      else{
+        ageYear.value = year-Number(getYear.value)-1;
+        ageMonth.value = month+(12-Number(getMonth.value));
+        ageDate.value = 30-(Number(getDate.value)-date);
+      }
+        
+  }
+    console.log(ageYear.value)
+    console.log(ageMonth.value)
+    console.log(ageDate.value)
+  }
+
+}
+  
+</script>
